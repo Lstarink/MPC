4f1867ad7578c1c4f849b7ea935db548b9cdfc21
lstar <l.starink@outlook.com>
2023-03-16 13:19:44 +0100

    Linearization works. Working on dt statespace

--- src/nonlinear_state_space_model.py
+++ src/nonlinear_state_space_model.py
@@ -1,6 +1,8 @@
 import sympy as sm
 import math as mt
+import numpy as np
 import config
+import scipy as sp
 
 
 class StateSpaceModel:
@@ -8,23 +10,51 @@ class StateSpaceModel:
         x, y, z, x_dot, y_dot, z_dot = sm.symbols("x, y, z, x_dot, y_dot, z_dot")
 
         # states
-        self.states = sm.Matrix([x, y, z, x_dot, y_dot, z_dot])
+        self.statesym = sm.Matrix([x, y, z, x_dot, y_dot, z_dot])
 
         c1, c2, c3, c4, c5, c6, c7, c8 = sm.symbols("c1, c2, c3, c4, c5, c6, c7, c8")
         # inputs
         self.inputs = sm.Matrix([c1, c2, c3, c4, c5, c6, c7, c8]).T
 
+        self.n = len(self.statesym)
+        self.m = len(self.inputs)
+
+        self.state = np.zeros(self.n)
+
         # state equation
         self.f = StateSpaceModel.F(self)
-        print(self.f.shape)
 
-    def Evaluate(self, state_numerical, input_numerical):
-        lambda_f = sm.utilities.lambdify([self.states, self.inputs], self.f)
+        self.lambdaA, self.lambdaB = StateSpaceModel.LinearizeInit(self)
+        self.A = np.zeros([self.n, self.n])
+        self.B = np.zeros([self.n, self.m])
+        self.C = np.identity(self.n)
+        self.D = np.zeros([self.n, self.m])
+        StateSpaceModel.Linearize(self)
+
+        self.ct_statespace = sp.signal.StateSpace(self.A, self.B, self.C, self.D)
+        self.dt = 0
+        self.dt_statespace
+
+
+    def NonlinearDiscreteTimeStep(self, input_n):
+        f_xu = StateSpaceModel.EvaluateNonlinear(self.state, input_n)
+        self.state += f_xu*self.dt
+
+    def LinearDiscreteTimeStep(self, input_n):
+        self.state = self.phi@self.state + self.gamma@input_n
+
+    def EvaluateNonlinear(self, state_numerical, input_numerical):
+        lambda_f = sm.utilities.lambdify([self.statesym, self.inputs], self.f)
         f_evaluated = lambda_f(state_numerical, input_numerical)
         return f_evaluated
 
+    def EvaluateNonLinearOnlyX(self, state_numerical):
+        psi = 0
+        theta = 0
+        return psi, theta
+
     def F(self):
-        f1 = sm.Matrix([self.states[3], self.states[4], self.states[5]])
+        f1 = sm.Matrix([self.statesym[3], self.statesym[4], self.statesym[5]])
         f2 = sm.Matrix([0, 0, -9.81])
         attachment_points = config.attachment_points
         for n, cable_tension in enumerate(self.inputs):
@@ -33,9 +63,40 @@ class StateSpaceModel:
         f = sm.Matrix([f1,f2])
         return f
 
+    def Linearize(self, x_eq=np.zeros(6), u_eq=np.zeros(8)):
+        self.A = self.lambdaA(x_eq, u_eq)
+        self.B = self.lambdaB(x_eq, u_eq)
+
+
+    def LinearizeInit(self):
+        A_ = sm.zeros(self.n)
+        B_ = sm.zeros(self.n, self.m)
+
+        for i, state_i in enumerate(self.statesym):
+            A_[:, i] = sm.diff(self.f, state_i)
+
+        for i, input_i in enumerate(self.inputs):
+            B_[:, i] = sm.diff(self.f, input_i)
+
+        lambda_A_ = sm.utilities.lambdify([self.statesym, self.inputs], A_)
+        lambda_B_ = sm.utilities.lambdify([self.statesym, self.inputs], B_)
+
+        return lambda_A_, lambda_B_
+
     def Force(self, cable_tension, cable_attachment_point):
-        location = sm.Matrix([self.states[0], self.states[1], self.states[2]])
+        location = sm.Matrix([self.statesym[0], self.statesym[1], self.statesym[2]])
         center_of_mass_to_attachment_point = sm.Matrix([cable_attachment_point]).T - location
         unit_vector = center_of_mass_to_attachment_point/(sm.sqrt(center_of_mass_to_attachment_point.dot(center_of_mass_to_attachment_point)))
         force = unit_vector*cable_tension
         return force
+
+    def CalculatePhiGamma(self):
+        self.Phi = 0
+        self.Gamma = 0
+
+    def Setdt(self, dt):
+        self.dt = dt
+        StateSpaceModel.CalculatePhiGamma(self)
+
+    def ResetState(self, state):
+        self.state = state
--- tests/test_nonlinear_state_space_model.py
+++ tests/test_nonlinear_state_space_model.py
@@ -2,19 +2,92 @@ import unittest
 import numpy as np
 import math as mt
 import nonlinear_state_space_model
+import matplotlib.pyplot as plt
 
 class TestStateSpace(unittest.TestCase):
-    def test(self):
+    def test_nonlinear_state_space_model_EvaluateNonlinear(self):
         state_space = nonlinear_state_space_model.StateSpaceModel()
-        f_evaluated1 = state_space.Evaluate(np.zeros(6), np.zeros(8))
-        f_evaluated2 = state_space.Evaluate(np.zeros(6), np.array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]))
-        f_evaluated3 = state_space.Evaluate(np.array([1, 0, 0, 0, 0, 0]), np.array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]))
-        print(f_evaluated3)
-
+        f_evaluated1 = state_space.EvaluateNonlinear(np.zeros(6),
+                                                     np.zeros(8))
+        f_evaluated2 = state_space.EvaluateNonlinear(np.zeros(6),
+                                                     np.array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]))
+        f_evaluated3 = state_space.EvaluateNonlinear(np.array([1, 0, 0, 0, 0, 0]),
+                                                     np.array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]))
         # zero input zero output
         self.assertEqual(np.linalg.norm(f_evaluated1), 9.81)
         # pull 4 strings located at the positive x side to generate acceleration in x direction and nothing else
         self.assertEqual(f_evaluated2[3], 4/mt.sqrt(3))
         # pull 4 strings located at the positive x side, with the mass in the same plane, so no acceleration
         self.assertEqual(np.linalg.norm(f_evaluated3 - f_evaluated1), 0)
+
+    def test_nonlinear_state_space_model_Linearize(self):
+        statespace = nonlinear_state_space_model.StateSpaceModel()
+        statespace.Linearize()
+        #linearized around 0
+        correctA = np.array([[0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
+
+        correctB1_lower_absolute = np.ones([3,8])/np.sqrt(3)
+
+        self.assertIsNone(np.testing.assert_array_equal(correctA, statespace.A))
+        self.assertIsNone(np.testing.assert_array_equal(np.zeros([3, 8]), statespace.B[0:3, :]))
+        self.assertIsNone(np.testing.assert_array_equal(correctB1_lower_absolute, abs(statespace.B[3:6, :])))
+
+        statespace.Linearize(x_eq=np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+
+        oversqrt2 = 1/np.sqrt(2)
+        oversqrt6 = 1/np.sqrt(6)
+
+        correctB2_lower_absolute = np.array([[0.0, 0.0, 0.0, 0.0, 2*oversqrt6, 2*oversqrt6, 2*oversqrt6, 2*oversqrt6],
+                                             [oversqrt2, oversqrt2, oversqrt2, oversqrt2, oversqrt6, oversqrt6, oversqrt6, oversqrt6],
+                                             [oversqrt2, oversqrt2, oversqrt2, oversqrt2, oversqrt6, oversqrt6, oversqrt6, oversqrt6]])
+
+        self.assertIsNone(np.testing.assert_array_equal(correctA, statespace.A))
+        self.assertIsNone(np.testing.assert_array_equal(np.zeros([3, 8]), statespace.B[0:3, :]))
+        self.assertIsNone(np.testing.assert_array_equal(correctB2_lower_absolute, abs(statespace.B[3:6, :])))
+
+        statespace.Linearize(x_eq=np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]), u_eq=np.random.rand(8))
+
+        self.assertIsNone(np.testing.assert_array_equal(np.zeros([3, 8]), statespace.B[0:3, :]))
+        self.assertIsNone(np.testing.assert_array_equal(correctB2_lower_absolute, abs(statespace.B[3:6, :])))
+
+    def test_ConvinceU(self):
+        statespace = nonlinear_state_space_model.StateSpaceModel()
+        statespace.Linearize(x_eq=np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+        A1 = statespace.A
+        B1 = statespace.B
+
+        statespace.Linearize(x_eq=np.array([0.5, 0.5, 0.5, 0.0, 0.0, 0.0]),
+                             u_eq=np.array([0.1, 0.1, 0.1, 0.1,
+                                            0.0, 0.0, 0.00, 0.0]))
+
+        A2 = statespace.A
+        B2 = statespace.B
+
+
+
+        fig, axs = plt.subplots(2)
+        fig.suptitle('Vertically stacked subplots')
+        cax1 = axs[0].matshow(B1, interpolation='nearest')
+        fig.colorbar(cax1)
+        cax2 = axs[1].matshow(B2, interpolation='nearest')
+        fig.colorbar(cax2)
+        axs[0].matshow(B1)
+        axs[1].matshow(B2)
+        fig.savefig('foo.png', bbox_inches='tight')
+
+        fig, axs = plt.subplots(2)
+        fig.suptitle('Vertically stacked subplots')
+        cax1 = axs[0].matshow(A1, interpolation='nearest')
+        fig.colorbar(cax1)
+        cax2 = axs[1].matshow(A2, interpolation='nearest')
+        fig.colorbar(cax2)
+        axs[0].matshow(A1)
+        axs[1].matshow(A2)
+        fig.savefig('foo2.png', bbox_inches='tight')
+
 
